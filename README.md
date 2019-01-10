583bc4e0fa55a7c89dce4e482fce3a441f672cab
mcekov <mcekovphoto@gmail.com>
2019-01-10 17:39:06 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # redditFinderApp
+
 Simple search app that uses reddit api.
+
+Starting app: \$ parcel index.html
---
+++ index.js
@@ -0,0 +1,84 @@
+import reddit from './redditapi';
+
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+
+searchForm.addEventListener('submit', e => {
+  // Get search term
+  const searchTerm = searchInput.value;
+  // Get sort
+  const sortBy = document.querySelector('input[name="sortby"]:checked').value;
+  //console.log(sortBy);
+  // Get limit
+  const searchLimit = document.getElementById('limit').value;
+
+  // Check input
+  if (searchTerm === '') {
+    showMessage('Please add a search term', 'alert-danger');
+  }
+
+  // Clear input
+  searchInput.value = '';
+
+  // Search Reddit
+  reddit.search(searchTerm, searchLimit, sortBy).then(results => {
+    let output = '<div class="card-columns">';
+    results.forEach(post => {
+      // Chek for image
+      const image = post.preview
+        ? post.preview.images[0].source.url
+        : 'https://cdn.comparitech.com/wp-content/uploads/2017/08/reddit-1.jpg';
+
+      output += `
+                  <div class="card">
+                  <img class="card-img-top" src="${image}" alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">${post.title}</h5>
+                    <p class="card-text">${truncate(post.selftext, 100)}</p>
+                    <a href="${
+                      post.url
+                    }" target="_blank" class="btn btn-primary">Read more</a>
+                    <hr>
+                    <span class="badge badge-secondary">Subreddit: ${
+                      post.subreddit
+                    }</span>
+                    <span class="badge badge-dark">Score: ${post.score}</span>
+                  </div>
+                </div>
+                `;
+    });
+    output += '</div>';
+    document.getElementById('results').innerHTML = output;
+  });
+
+  e.preventDefault();
+});
+
+// Show message
+function showMessage(message, className) {
+  // Create div
+  let div = document.createElement('div');
+  // Add classes
+  div.className = `alert ${className}`;
+  // Add text
+  div.appendChild(document.createTextNode(message));
+  // Get parent container
+  const searchContainer = document.getElementById('search-container');
+  // Get search
+  const search = document.getElementById('search');
+
+  // Insert message
+  searchContainer.insertBefore(div, search);
+
+  // Timeout alert
+  setTimeout(() => document.querySelector('.alert').remove(), 3000);
+}
+
+// Truncate text
+function truncate(text, limit) {
+  const shortened = text.indexOf(' ', limit);
+  if (shortened === -1) {
+    return text;
+  }
+  return text.substring(0, shortened);
+}
